6845c5dc2bafb5f486fbbfa728d25669a7bd4398
titlo10 <titlo10rebooting@gmail.com>
2025-02-22 22:27:54 +0300

    create a bottom navigation bar

--- app/src/main/java/com/example/createplanetapp/MainActivity.kt
+++ app/src/main/java/com/example/createplanetapp/MainActivity.kt
@@ -3,12 +3,30 @@ package com.example.createplanetapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.example.createplanetapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(2000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadFragment(Home())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottom_home -> loadFragment(Home())
+                R.id.bottom_settings -> loadFragment(Settings())
+                R.id.bottom_favorites -> loadFragment(Favorites())
+                else -> {}
+            }
+            true
+        }
     }
+
+    private fun loadFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
 }
