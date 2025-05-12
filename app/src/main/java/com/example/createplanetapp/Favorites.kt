dce42d38a8b1fcf48744c00d7ba9a6411e3f0b77
ArtemAbrosimov1 <abrosimov_ad@mail.ru>
2025-05-12 22:52:08 +0300

    Page "Favorites" changes

--- app/src/main/java/com/example/createplanetapp/Favorites.kt
+++ app/src/main/java/com/example/createplanetapp/Favorites.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Favorites : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,25 @@ class Favorites : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_favorites, container, false)
+
+        val itemsList: RecyclerView = view.findViewById(R.id.favorites_list)
+        val items = csvParser(resources, R.raw.po_gorodu)
+        items.addAll(csvParser(resources, R.raw.zagorodnie))
+
+        val dbHelper = DBHelper(requireContext(), null)
+        val favoriteItems = dbHelper.getRecords(items, true, false)
+
+        itemsList.layoutManager = LinearLayoutManager(context)
+        itemsList.adapter = FavoritesAdapter(favoriteItems, requireContext())
+
+        val emptyText: TextView = view.findViewById(R.id.empty_favorites_txt)
+        if (favoriteItems.isEmpty()) {
+            emptyText.visibility = View.VISIBLE
+            itemsList.visibility = View.GONE
+        } else {
+            emptyText.visibility = View.GONE
+            itemsList.visibility = View.VISIBLE
+        }
 
         return view
     }
