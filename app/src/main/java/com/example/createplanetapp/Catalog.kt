25a73fcf765e39a78bcdab4a6602e7fbeb2e629e
mrkv 4449 <markoffleonid@mail.ru>
2025-05-13 22:18:26 +0300

    commit 13.05

--- app/src/main/java/com/example/createplanetapp/Catalog.kt
+++ app/src/main/java/com/example/createplanetapp/Catalog.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Catalog : Fragment() {
     override fun onCreateView(
@@ -13,6 +15,11 @@ class Catalog : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_catalog, container, false)
         val data = arguments?.getParcelableArrayList<ItemsViewModel>("dataTest") ?: arrayListOf()
+
+        //val spinner: Spinner = view.findViewById(R.id.catalog_options_spinner)
+        //var tourOptions = arrayOf("Школьникам", "Семьям")
+        //var adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, tourOptions)
+
 
         return view
     }
