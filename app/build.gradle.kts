79c9a531739888cc12c1e9b6485cd0377fdad3fd
Nao2705 <pluzhnik.ad@gmail.com>
2025-05-16 01:11:26 +0300

    Adding Home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("io.coil-kt:coil:2.4.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
