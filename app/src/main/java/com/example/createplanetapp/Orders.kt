218fcbcb42b122fbb4d54eb9ab5adbf098f3d0b7
Valentin Golikov <triderm300@gmai.com>
2025-05-16 12:36:19 +0300

    Add order's logic

--- app/src/main/java/com/example/createplanetapp/Orders.kt
+++ app/src/main/java/com/example/createplanetapp/Orders.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,8 +20,20 @@ class Orders : Fragment() {
         val items = csvParser(resources, R.raw.po_gorodu)
         items.addAll(csvParser(resources, R.raw.zagorodnie))
 
+        val dbHelper = DBHelper(requireContext(), null)
+        val orderedItems = dbHelper.getRecords(items, false, true)
+
         itemsList.layoutManager = LinearLayoutManager(context)
-        itemsList.adapter = OrdersAdapter(items, requireContext())
+        itemsList.adapter = OrdersAdapter(orderedItems, requireContext())
+
+        val emptyText: TextView = view.findViewById(R.id.empty_orders_txt)
+        if (orderedItems.isEmpty()) {
+            emptyText.visibility = View.VISIBLE
+            itemsList.visibility = View.GONE
+        } else {
+            emptyText.visibility = View.GONE
+            itemsList.visibility = View.VISIBLE
+        }
 
         return view
     }
